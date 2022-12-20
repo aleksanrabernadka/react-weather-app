ec0725cf62ab8ed76565ea340cd48f3b2ef188a2
aleksanrabernadka <106868378+aleksanrabernadka@users.noreply.github.com>
2022-12-20 13:38:59 +0100

    added link on GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        <a
+          href="https://github.com/aleksanrabernadka/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced
+        </a>
+        on Github
+      </footer>
+    </div>
+  );
 }
