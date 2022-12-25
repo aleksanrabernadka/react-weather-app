ab7c6a3e0e499d78c4005a3111eea0cdc5040fa6
aleksanrabernadka <106868378+aleksanrabernadka@users.noreply.github.com>
2022-12-26 00:28:57 +0100

    fixed 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           <a
             href="https://github.com/aleksanrabernadka/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             Open-sourced
           </a>
